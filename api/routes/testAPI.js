5530b8e0e7660a3923a9b0793920a0a65b4e8d6d
Ian Page <Ian.Page@dal.ca>
2021-02-02 19:21:37 -0400

    got email notifier working

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 router.get('/', function(req, res, next) {
     res.send('API is working properly');
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'surf.forecast.alert',
+            pass: 'SurfPassword'
+        }
+    });
+      
+    var mailOptions = {
+        from: 'surf.forecast.alert@gmail.com',
+        to: 'ian1239998@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+      
+    // transporter.sendMail(mailOptions, function(error, info){
+    //     if (error) {
+    //         console.log(error);
+    //     } else {
+    //         console.log('Email sent: ' + info.response);
+    //     }
+    // });
 });
 
 module.exports = router;
