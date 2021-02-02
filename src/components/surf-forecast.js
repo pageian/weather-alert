19bb5eb9cd2614b6d90f0b3f6c156bf3e357a3ef
Ian Page <Ian.Page@dal.ca>
2021-02-02 16:58:16 -0400

    got surf forecast working

--- src/components/surf-forecast.js
+++ src/components/surf-forecast.js
@@ -3,36 +3,54 @@ var moment = require('moment');
 
 class SurfForecast extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            forecast: []
+        }
+    }
+
+    maxPeriod(swells) {
+        
+        // trimming empty records
+        for(var i = 0; i < swells.length; i++){
+            if(swells[i].height == 0) {
+                swells.splice(i, 1);
+            }
+        }
+
+        // retreiving main swell
+        var maxi = -1;
+        var maxheight = -1;
+        for(var i = 0; i < swells.length; i++){
+            if(swells[i].height > maxheight) {
+                maxi = i;
+                maxheight = swells[i].height;
+            }
+        }
+
+        return swells[maxi].period
+    }
+
     render () {
 
-        if (!this.state.forecast || !this.state.forecast.data || !this.state.forecast.data.wave ) {
+        if (!this.state.forecast || !this.state.forecast.data || !this.state.forecast.data.wave) {
             return <span>Loading...</span>;
-          }
+        }
+
+        var periods = []
+
 
         return (
-            // <div>
-            //     <div class="card">
-            //         <div class="card-body">
-            //             <h5 class="card-title">{this.state.weather.timezone}</h5>
-            //             <h6 class="card-subtitle mb-2 text-muted">{this.state.weather.current.weather[0].main}</h6>
-            //             <p class="card-text">
-            //             temp: {this.state.weather.current.temp} C <br/>
-            //             feels like: {this.state.weather.current.feels_like} C
-            //             </p>
-            //             {/* <p class="card-text">feels like: {this.state.weather.current.feels_like} C</p> */}
-            //         </div>
-            //     </div>
-            //     <Forecast forecast={ this.state.weather.daily } /> 
-            // </div>
             <div>
                 <center><div>Surf Forcast</div></center>
                 {this.state.forecast.data.wave.map((f) => (
                     <div key={f.timestamp} class="card">
                         <div class="card-body">
                             <h6 class="card-subtitle mb-2 text-muted">{ moment(new Date(f.timestamp * 1000)).format('ddd, MMM D') }</h6>
-                            <h5 class="card-title">{f.surf.min} - {f.surf.max}</h5>
-                            {/* <h6 class="card-subtitle mb-2 text-muted">{w.weather[0].description}</h6>
-                            <p class="card-text">
+                            <h5 class="card-title">{f.surf.min}-{f.surf.max}m @ {this.maxPeriod(f.swells)}s</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{f.surf.optimalScore}*</h6>
+                            {/* <p class="card-text">
                                 max: {w.temp.max} C <br/>
                                 min: {w.temp.min} C</p> */}
                         </div>
@@ -42,21 +60,17 @@ class SurfForecast extends Component {
         );
     }
 
-    state = {
-        forecast: []
-    }
-
     componentDidMount() {
         // lawrencetown forecast
         fetch('https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=584204204e65fad6a77094cf&intervalHours=24')
         .then(res => res.json())
         .then((data) => {
-            this.setState({ forecast: data })
+            this.setState(
+                { forecast: data })
             console.log(data)
         })
         .catch(console.log)
-    }
-    
+    } 
 };
 
 export default SurfForecast;
