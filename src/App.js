b755040fffcd38a0b4ed09c61f67f1bb5986488d
Ian Page <ian.am.page@gmail.com>
2021-02-03 16:15:03 -0400

    added cookie based lock out

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, {Component} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
 import Home from './components/home';
 import SurfForecast from './components/surf-forecast';
@@ -10,10 +11,29 @@ import Login from './components/login';
 import Signup from './components/signup';
 
 class App extends Component {
-  render () {
+  
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
 
-    return (
-      <BrowserRouter>
+  render () {
+    const cookies = new Cookies();
+    if(cookies.get('uid') == "" || !cookies.get('uid')){
+      return (
+        <div>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/surf" component={SurfForecast} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+          <Redirect to="/login" />
+        </div>
+      );
+    } else {
+      return (
         <div>
           <Switch>
             <Route path="/" component={Home} exact />
@@ -22,8 +42,8 @@ class App extends Component {
             <Route path="/signup" component={Signup} />
           </Switch>
         </div>
-      </BrowserRouter>
-    );
+      );
+    }
   }
 }
 
