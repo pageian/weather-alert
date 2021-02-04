57d2c46a6f6743e84e39a7d19287eeb831b3833a
Ian Page <ian.am.page@gmail.com>
2021-02-04 11:19:37 -0400

    fields updatable

--- src/components/user.js
+++ src/components/user.js
@@ -8,8 +8,24 @@ class User extends Component {
     constructor () {
         super();
         this.state = {
-            settings: []
+            name: "",
+            min_height: 0,
+            max_height: 0,
+            min_period: 0,
+            max_period: 0,
+            metric: true,
+            notifs: false
         }
+    }
+
+    updateField (event) {
+        if(event.target.name == 'user_name') this.setState({name: event.target.value});
+        else if(event.target.name == 'min_height') this.setState({min_height: event.target.value});
+        else if(event.target.name == 'max_height') this.setState({max_height: event.target.value});
+        else if(event.target.name == 'min_period') this.setState({min_period: event.target.value});
+        else if(event.target.name == 'max_period') this.setState({max_period: event.target.value});
+        else if(event.target.name == 'notifs') this.setState({notifs: event.target.value});
+        else if(event.target.name == 'metric') this.setState({metric: event.target.value});
     }
 
     render () {
@@ -17,27 +33,32 @@ class User extends Component {
             <div>
                 <h1>Settings</h1>
                 <Form>
+                    <Form.Group controlId="formName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" name="user_name" value={this.state.name} onChange={this.updateField.bind(this)} />
+                    </Form.Group>
+
                     <Form.Group controlId="formWaveHeight">
                         <Form.Label>Min. wave height</Form.Label>
-                        <Form.Control type="number" value={this.state.settings.min_height} />
+                        <Form.Control type="number" name="min_height" value={this.state.min_height} onChange={this.updateField.bind(this)} />
                         <Form.Label>Max. wave height</Form.Label>
-                        <Form.Control type="number" value={this.state.settings.max_height} />
+                        <Form.Control type="number" name="max_height" value={this.state.max_height} onChange={this.updateField.bind(this)} />
                     </Form.Group>
 
                     <Form.Group controlId="formWavePeriod">
                         <Form.Label>Min. wave period</Form.Label>
-                        <Form.Control type="number" value={this.state.settings.min_period} />
+                        <Form.Control type="number" name="min_period" value={this.state.min_period} onChange={this.updateField.bind(this)} />
                         <Form.Label>Max. wave period</Form.Label>
-                        <Form.Control type="number" value={this.state.settings.max_period} />
+                        <Form.Control type="number" name="max_period" value={this.state.max_period} onChange={this.updateField.bind(this)} />
                     </Form.Group>
 
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" checked={this.state.settings.notifs} label="activate notifications" />
+                    <Form.Group controlId="formNotifs">
+                        <Form.Check type="checkbox" name="notifs" checked={this.state.notifs} onChange={this.updateField.bind(this)} label="activate notifications" />
                     </Form.Group>
 
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="radio" checked={this.state.settings.metric} label="metric" />
-                        <Form.Check type="radio" label="imperial" />
+                    <Form.Group controlId="formUnits">
+                        <Form.Check type="radio" name="" checked={this.state.metric} onChange={this.updateField.bind(this)} label="metric" />
+                        <Form.Check type="radio" name="" label="imperial" />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
@@ -54,8 +75,15 @@ class User extends Component {
         .then(res => res.json())
         .then((data) => {
             console.log(data);
-            this.setState({settings: data})
-            console.log(this.state.settings)
+            this.setState({
+                name: data.name,
+                min_height: data.min_height,
+                max_height: data.max_height,
+                min_period: data.min_period,
+                max_period: data.max_period,
+                metric: data.metric,
+                notifs: data.notifs
+            });
         });
     }
 }
