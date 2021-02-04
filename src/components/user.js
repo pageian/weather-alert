a47741cffe4543f0c60c9c919095deecfcb705c4
Ian Page <ian.am.page@gmail.com>
2021-02-04 10:14:12 -0400

    added settings form

--- src/components/user.js
+++ src/components/user.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 class User extends Component {
     
@@ -13,6 +15,28 @@ class User extends Component {
         return(
             <div>
                 <h1>Settings</h1>
+                <Form>
+                    <Form.Group controlId="formWaveHeight">
+                        <Form.Label>Wave height</Form.Label>
+                        <Form.Control type="number" placeholder="min. wave height" />
+                        <Form.Control type="number" placeholder="max. wave height" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formWavePeriod">
+                        <Form.Label>Wave period</Form.Label>
+                        <Form.Control type="number" placeholder="min. wave period" />
+                        <Form.Control type="number" placeholder="max. wave period" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="radio" label="metric" />
+                        <Form.Check type="radio" label="imperial" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+
             </div>
         );
     }
