d8d69bbac8b37dc925c68de616a902b5ab06836b
Ian Page <ian.page@innovasea.com>
2021-12-21 22:30:10 -0400

    update api logs

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -177,32 +177,36 @@ router.get('/surfspots', function(req, res) {
 });
 
 router.get('/surfdata', function(req, res) {
-
+    console.log('GETTING SUF DATA');
     var int_hour = 24;
     var data = {};
 
     //surf forecast
     request('https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=' + req.query.spot_id + '&intervalHours=' + int_hour, function (err0, res0, body0) {
         if (!err0 && res0.statusCode == 200) {
-            console.log(JSON.parse(body0));
+            // console.log(JSON.parse(body0));
+            console.log('GOT WAVE DATA');
             data.waves = JSON.parse(body0).data.wave;
 
             // tides query
             request('https://services.surfline.com/kbyg/spots/forecasts/tides?spotId=' + req.query.spot_id + '&intervalHours=' + int_hour, function (err1, res1, body1) {
                 if (!err1 && res1.statusCode == 200) {
-                    console.log(JSON.parse(body1));
+                    // console.log(JSON.parse(body1));
+                    console.log('GOT TIDE DATA');
                     data.tides = JSON.parse(body1).data.tides;
 
                     // wind query
                     request('https://services.surfline.com/kbyg/spots/forecasts/wind?spotId=' + req.query.spot_id + '&intervalHours=' + int_hour, function (err2, res2, body2) {
                         if (!err2 && res2.statusCode == 200) {
-                            console.log(JSON.parse(body2));
+                            // console.log(JSON.parse(body2));
+                            console.log('GOT WIND DATA');
                             data.winds = JSON.parse(body2).data.wind;
 
                             // weather query
                             request('https://services.surfline.com/kbyg/spots/forecasts/weather?spotId=' + req.query.spot_id + '&intervalHours=' + int_hour, function (err3, res3, body3) {
                                 if (!err3 && res3.statusCode == 200) {
-                                    console.log(JSON.parse(body3));
+                                    // console.log(JSON.parse(body3));
+                                    console.log('GOT WEATHER DATA');
                                     data.weather = JSON.parse(body3).data;
 
                                     res.send(data);
